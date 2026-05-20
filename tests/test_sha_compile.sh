945e4d8349d92ada1771559c9fce6ae1766ac7d7
vanhieupc <ngovanhieu2006pc@gmail.com>
2026-05-21 01:05:28 +0700

    Update test_sha_compile.sh

--- tests/test_sha_compile.sh
+++ tests/test_sha_compile.sh
@@ -12,3 +12,4 @@ g++ -std=c++17 -Wall -Wextra -pedantic salted_password_hash.cpp -o salted_passwo
 [[ -x ./salted_password_hash ]] || { echo "[FAIL] Missing salted_password_hash executable"; exit 1; }
 
 echo "[PASS] SHA programs compile successfully."
+#vh
